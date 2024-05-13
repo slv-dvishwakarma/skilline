93f4098ca6cd49aa0f0b356adc0479f2624fa5a8
slv-ymaharaju <slv-ymaharaju@skilline.ai>
2024-05-13 12:32:43 +0530

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import "./globals.css"; 
+import "./globals.css";
 import { ParentContainer } from "@/components/ParentContainer";
 import { Header } from "@/components/template/Header";
 import { Footer } from "@/components/template/Footer";
@@ -17,11 +17,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
-        /> 
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto"
+          rel="stylesheet"
+        />
+        <link
+          href="https://cdn.quilljs.com/1.0.0/quill.snow.css"
+          rel="stylesheet"
+        />
+
+        <script src="https://cdn.quilljs.com/1.0.0/quill.js"></script>
       </head>
       <body>
         <ParentContainer className="bg-primary">
--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,51 +1,114 @@
-import React, { useRef, useEffect } from 'react';
-import ReactQuill, { Quill } from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, { useRef, useEffect } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 // Define custom fonts
-const Font = Quill.import('formats/font');
-Font.whitelist = ['Arial', 'Verdana', 'Times New Roman', 'Georgia', 'Courier New'];
+const Font = Quill.import("formats/font");
+Font.whitelist = [
+  "Roboto",
+  "Raleway",
+  "Montserrat",
+  "Lato",
+  "Rubik",
+  "Arial",
+  "Verdana",
+  "Helvetica",
+  "Tahoma",
+  "Trebuchet MS",
+  "Georgia",
+  "Times New Roman",
+  "Courier New",
+  "Lucida Console",
+  "Impact",
+  "Arial Black",
+  "Comic Sans MS",
+  "Tahoma",
+  "Geneva",
+  "Garamond",
+  "Bookman",
+  "Courier",
+  "Century Schoolbook",
+  "Palatino",
+  "Times",
+  "Arial Narrow",
+  "Gadget",
+  "Copperplate",
+  "Papyrus",
+  "Brush Script MT",
+  "Segoe UI",
+  "Arial Unicode MS",
+  "Calibri",
+  "Candara",
+  "Consolas",
+  "Constantia",
+  "Corbel",
+  "Ebrima",
+  "Franklin Gothic Medium",
+  "Gabriola",
+  "Ink Free",
+  "Javanese Text",
+  "Leelawadee UI",
+  "Microsoft Himalaya",
+  "Microsoft JhengHei",
+  "Microsoft YaHei",
+  "Miriam",
+  "Myanmar Text",
+  "Nirmala UI",
+  "Segoe Script",
+  "SimSun",
+  "Sitka",
+  "Sylfaen",
+  "Trebuchet MS",
+  "Verdana",
+];
 Quill.register(Font, true);
 
 interface QuillEditorProps {
-    value: string;
-    onChange: (content: string) => void;
+  value: string;
+  onChange: (content: string) => void;
 }
 
-export const QuillEditor: React.FC<QuillEditorProps> = ({ value, onChange }) => {
-    const quillRef = useRef<ReactQuill | null>(null);
+export const QuillEditor: React.FC<QuillEditorProps> = ({
+  value,
+  onChange,
+}) => {
+  const quillRef = useRef<ReactQuill | null>(null);
 
-    useEffect(() => {
-        if (quillRef.current) {
-            const editor = quillRef.current.getEditor();
-            if (editor) {
-                editor.on('text-change', () => {
-                    const content = quillRef.current?.getEditor().root.innerHTML || "";
-                    onChange(content);
-                });
-            }
-        }
-    }, [onChange]);
+  useEffect(() => {
+    if (quillRef.current) {
+      const editor = quillRef.current.getEditor();
+      if (editor) {
+        editor.on("text-change", () => {
+          const content = quillRef.current?.getEditor().root.innerHTML || "";
+          onChange(content);
+        });
+      }
+    }
+  }, [onChange]);
 
-    const modules = {
-        toolbar: {
-            container: [
-                [{ 'header': '1' }, { 'header': '2' }, { 'font': Font.whitelist }],
-                [{ 'size': ['small', false, 'large', 'huge'] }],
-                ['bold', 'italic', 'underline'],
-                ['link', 'image'],
-                ['clean']
-            ]
-        }
-    };
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, 3, 4, 5, 6] }],
+        [{ font: Font.whitelist }],
+        [{ size: ["small", false, "large", "huge"] }],
+        ["bold", "italic", "underline", "strike"],
+        [{ color: [] }, { background: [] }],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        ["link", "image", "video"],
+        ["clean"],
+      ],
+    },
+  };
 
-    return (
-        <ReactQuill
-            ref={quillRef}
-            theme="snow"
-            value={value || ""}
-            onChange={onChange}
-            modules={modules}
-        />
-    );
+  return (
+    <ReactQuill
+      ref={quillRef}
+      theme="snow"
+      value={value || ""}
+      onChange={onChange}
+      modules={modules}
+    />
+  );
 };
